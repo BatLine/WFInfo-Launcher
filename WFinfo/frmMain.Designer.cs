62a37a78627684c6ee21350a1077c8f20909f340
Gilles De Pessemier <gilles.depessemier@gmail.com>
2020-09-04 19:14:31 +0200

    Initial Commit

---
+++ WFinfo/frmMain.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace WFinfo
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tmrCheck = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // tmrCheck
+            // 
+            this.tmrCheck.Interval = 5000;
+            this.tmrCheck.Tick += new System.EventHandler(this.tmrCheck_Tick);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(120, 14);
+            this.Name = "frmMain";
+            this.Text = "Form1";
+            this.TransparencyKey = System.Drawing.SystemColors.Control;
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer tmrCheck;
+    }
+}
+
---
+++ WFinfo/frmMain.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace WFinfo
+{
+    public partial class frmMain : Form
+    {
+        public frmMain() { InitializeComponent(); }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            hide();
+            runWarframe();
+            tmrCheck.Start();
+        }
+
+        private void tmrCheck_Tick(object sender, EventArgs e)
+        {
+            if (!checkIfWarframeIsRunning())
+            {
+                Thread.Sleep(2000);
+                if (!checkIfWarframeIsRunning())
+                {
+                    if (checkIfWFInfoIsRunning())
+                    {
+                        stopWFInfo();
+                        tmrCheck.Stop();
+                        Application.Exit();
+                    }
+                }
+            } else
+            {
+                if (checkIfProcessIsRunning("warframe.x64"))
+                {
+                    if (!checkIfWFInfoIsRunning())
+                    {
+                        Thread.Sleep(5000);
+                        runWFinfo();
+                    }
+                }
+            }
+        }
+
+        private void runWarframe() { Process.Start("steam://rungameid/230410"); }
+
+        private void runWFinfo()
+        {
+            try {
+                Process.Start(Application.StartupPath + @"\WFInfo.exe");
+            } catch (Exception) {
+                MessageBox.Show("Error starting wfinfo, make sure it is in the same directory.");
+                Application.Exit();
+            }
+        }
+
+        private bool checkIfWarframeIsRunning() {
+            if ((checkIfProcessIsRunning("warframe")) || (checkIfProcessIsRunning("launcher")) || (checkIfProcessIsRunning("warframe.x64")))
+                return true;
+            return false;
+        }
+
+        private bool checkIfWFInfoIsRunning() { return checkIfProcessIsRunning("wfinfo"); }
+
+        private void stopWFInfo() {
+            foreach(Process _p in Process.GetProcessesByName("wfinfo")) {
+                try {
+                    _p.Kill();
+                } catch (Exception) { }
+            }
+        }
+
+        private bool checkIfProcessIsRunning(string pName)
+        {
+            Process[] pname = Process.GetProcessesByName(pName);
+            if (pname.Length > 0)
+                return true;
+            return false;
+        }
+
+        private void hide()
+        {
+            //makes window invisible
+            this.WindowState = FormWindowState.Minimized;
+            this.Visible = false;
+            this.Size = new Size(0, 0);
+            this.Opacity = 0;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.ControlBox = false;
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Hide();
+        }
+    }
+}
